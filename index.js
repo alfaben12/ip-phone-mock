e3c46c5ffdfad4a1891ed0a896d64acabdaeccf7
Thariq Alfa <zrav420@gmail.com>
2024-09-20 10:09:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+var cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 8080;
