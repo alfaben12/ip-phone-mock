72e6ac875ba597def475b0925295aa06e8dc4c23
Thariq Alfa <thariq.benriska@mandirisekuritas.co.id>
2024-12-15 19:22:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-var cors = require('cors')
+var cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(require("body-parser").urlencoded({ extended: false }));
-
-const pathFile = path.join(process.cwd(), "/", "data.json");
-
-const endpoints = JSON.parse(fs.readFileSync(pathFile, "utf8"));
 app.get("/", (req, res) => {
   res.send("Welcome to mock!");
 });
+
+const filePath = path.join(__dirname, "data.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const endpoints = JSON.parse(data);
 
 function getResponse(req, res, data) {
   return res.status(200).json(data);
@@ -23,32 +23,11 @@ function getResponse(req, res, data) {
 
 for (let i = 0; i < endpoints.length; i++) {
   const endpoint = endpoints[i].endpoint;
-  const method = endpoints[i].method;
   const data = endpoints[i].data;
 
-  if (method == "POST") {
-    app.post(endpoint, (req, res) => {
-      console.log("post body is " + JSON.stringify(req.body));
-      return getResponse(req, res, data);
-    });
-  } else if (method == "PUT") {
-    console.log("put body is " + JSON.stringify(req.body));
-    app.put(endpoint, (req, res) => {
-      return getResponse(req, res, data);
-    });
-  } else if (method == "GET") {
-    app.get(endpoint, (req, res) => {
-      return getResponse(req, res, data);
-    });
-  } else if (method == "DELETE") {
-    app.delete(endpoint, (req, res) => {
-      return getResponse(req, res, data);
-    });
-  } else {
-    app.post(endpoint, (req, res) => {
-      return getResponse(req, res, data);
-    });
-  }
+  app.post(endpoint, (req, res) => {
+    return getResponse(req, res, data);
+  });
 }
 
 app.listen(port, () => {
